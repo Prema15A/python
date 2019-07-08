a9c3953c17f6ce4c8e451b5a18a38ebdc91e7964
Prema15A <52250360+Prema15A@users.noreply.github.com>
2019-07-08 12:11:38 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,10 @@
+gh=int(input())
+if gh > 1:
+    for ds in range(2,gh):
+        if(gh%ds==0):
+            print('no')
+            break
+    else:
+            print('yes')
+    else:
+            print('no')
