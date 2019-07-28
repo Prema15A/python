13f1211c70c875294f4da911686372f7c8fe5230
Prema15A <52250360+Prema15A@users.noreply.github.com>
2019-07-28 09:19:17 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,7 +1,7 @@
-a,b,c=1,2,3
-if(a>b and a>c):
-  print(a)
-elif(b>a and b>c):
-  print(b)
+nom1,nom2,nom3=input().split()
+if(nom1>nom2 and nom1>nom3):
+    print("n1")
+elif(nom2>nom3):
+    print("n2")
 else:
-  print(c)  
+    print("n3")
