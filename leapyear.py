c0e503f4637a868b3ca5a797f3d70f1a2e0598d9
Prema15A <52250360+Prema15A@users.noreply.github.com>
2019-07-28 09:22:37 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,12 @@
-y=2016
-if y%4==0:
- print(yes)
+yers=int(input())
+if(yers % 4)==0:
+    if(yers % 100)==0:
+        if(yers % 400)==0:
+            print("yes")
+        else:
+            print("no")
+    else:
+        print("yes")
 else:
- print(no)
+    print("no")
+        
