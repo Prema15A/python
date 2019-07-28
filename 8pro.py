382c36f9ec4c70a40d1749531130f3c693cc1071
Prema15A <52250360+Prema15A@users.noreply.github.com>
2019-07-28 18:11:51 +0530

    Update 8pro.py

--- 8pro.py
+++ 8pro.py
@@ -1,11 +1,11 @@
 import math
-prem,ser=map(int,input().split())
-srww=[]
-bob=list(map(int,input().split()))
-for i in range(0,ser):
-    lcc,hcc=map(int,input().split())
-    srww.append([lcc,hcc])
-for i in srww:
-    din=i[0]-1
-    eit=i[1]-1
-    print(math.gcd(bob[din],bbb[eit]))
+prem,sup=map(int,input().split())
+smi=[]
+ber=list(map(int,input().split()))
+for i in range(0,sup):
+    loc,hoc=map(int,input().split())
+    smi.append([loc,hoc])
+for i in smi:
+    don=i[0]-1
+    eot=i[1]-1
+    print(math.gcd(ber[don],ber[eot]))
