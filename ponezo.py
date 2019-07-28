f7d59d2f997a9197e39a6d74e9fb2239e7df9f8a
Prema15A <52250360+Prema15A@users.noreply.github.com>
2019-07-28 08:35:23 +0530

    Update ponezo.py

--- ponezo.py
+++ ponezo.py
@@ -1,7 +1,7 @@
-num=float(input("Enter a number:"))
-if num>0:
-  print("Positive number")
-elif num==0:
-  print("Zero")
+pqr=int(input())
+if(pqr<0):
+    print("Negative")
+elif(pqr>0):
+    print("Positive")
 else:
-  print("Negative number")
+        print("Zero")
