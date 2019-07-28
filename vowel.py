b899876a89c46504596f214b2e1b8de40a5111b6
Prema15A <52250360+Prema15A@users.noreply.github.com>
2019-07-28 09:12:01 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,6 @@
-char=input()
-if char.isalpha():
-    if(char=='A'or char=='E'or char=='I'or char=='O'or char=='U'or char=='a'or char=='e'or char=='i'or char=='o'or char=='u'):
+cat=input()
+if cat.isalpha():
+    if(cat=='A'or cat=='E'or cat=='I'or cat=='O'or cat=='U'or cat=='a'or cat=='e'or cat=='i'or cat=='o'or cat=='u'):
           print("Vowel")
     else:
           print("Consonant")
