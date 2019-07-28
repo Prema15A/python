a4547aee910307abd25a346e41f746064c953885
Prema15A <52250360+Prema15A@users.noreply.github.com>
2019-07-28 08:40:04 +0530

    Update evenodd.py

--- evenodd.py
+++ evenodd.py
@@ -1,7 +1,10 @@
-a=222
-if(a%2==0):
-  print("Even")
+pqr=int(input())
+if(pqr>0):
+    if(pqr%2==0):
+        print("Even")
+    else:
+        print("Odd")
 else:
-  print("Odd")
+    print("invalid")
 
 
