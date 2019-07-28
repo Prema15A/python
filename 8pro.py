8880971a5c845c0779a24056a7b73bceebf669a8
Prema15A <52250360+Prema15A@users.noreply.github.com>
2019-07-28 18:02:10 +0530

    Create 8pro.py

---
+++ 8pro.py
@@ -0,0 +1,11 @@
+import math
+prem,ser=map(int,input().split())
+srio=[]
+bob=list(map(int,input().split()))
+for i in range(0,ser):
+    lcc,hcc=map(int,input().split())
+    srio.append([lcc,hcc])
+for i in sroi:
+    din=i[0]-1
+    eit=i[1]-1
+    print(math.gcd(bob[din],bbb[eit]))
