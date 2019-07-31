a21df390fe4bcae34ad8ee707bf0cbd66d84af8d
Prema15A <52250360+Prema15A@users.noreply.github.com>
2019-07-31 20:21:48 +0530

    Create 21pro.py

---
+++ 21pro.py
@@ -0,0 +1,9 @@
+prem=int(input())
+maxi=list(map(int,input().split()))
+answ=int(prem/2)
+ran=maxi[:answ]
+mon=maxi[answ::]
+if ((sum(ron)//len(ron))==(sum(mon)//len(mon))):
+    print("yes")
+else:
+    print("no")
