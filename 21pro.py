1ec220f10e0498098ec3bb7a7e68a7cc301e6c7b
Prema15A <52250360+Prema15A@users.noreply.github.com>
2019-07-31 20:47:14 +0530

    Update 21pro.py

--- 21pro.py
+++ 21pro.py
@@ -1,9 +1,9 @@
-prem=int(input())
-maxi=list(map(int,input().split()))
-answ=int(prem/2)
-run=maxi[:answ]
-mon=maxi[answ::]
-if ((sum(run)//len(run))==(sum(mon)//len(mon))):
+prems=int(input())
+maxm=list(map(int,input().split()))
+anse=int(prems/2)
+rtn=maxm[:anse]
+mkn=maxm[anse::]
+if ((sum(rtn)//len(rtn))==(sum(mkn)//len(mkn))):
     print("yes")
 else:
     print("no")
