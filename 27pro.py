6d130422304ad545239a8da312ee72d0cb61266f
Prema15A <52250360+Prema15A@users.noreply.github.com>
2019-08-04 20:08:48 +0530

    Update 27pro.py

--- 27pro.py
+++ 27pro.py
@@ -2,16 +2,16 @@ prem,sds=map(int,input().split())
 zvz=list(map(int,input().split()))
 vmb=list(map(int,input().split()))
 tkn=[]
-pin=0
+uin=0
 for i in range(prem):
     x=vmb[i]/zvz[i]
     tkn.append(x)
 while sds>=0 and len(tkn)>0:
     mindex=tkn.index(max(tkn))
     if sds>=zvz[mindex]:
-        pin=pin+vmb[mindex]
+        uin=uin+vmb[mindex]
         sds=sds-zvz[mindex]
     zvz.pop(mindex)
     vmb.pop(mindex)
     tkn.pop(mindex)
-print(cin)
+print(uin)
